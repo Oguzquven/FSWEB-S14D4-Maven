75a3c4085bd69dfe055ef50b91b082a9cd039ae9
Oguzquven <oguzquven70@gmail.com>
2026-04-24 16:35:49 +0300

    Java Abstraction - ProductForSale & Monster RPG tasks completed

--- src/main/java/org/example/model/Store.java
+++ src/main/java/org/example/model/Store.java
@@ -1,12 +1,20 @@
 package org.example.model;
 
 public class Store {
-    public static void main(String[] args) {
-
-        //listProducts();
-    }
 
     public static void listProducts(ProductForSale[] products) {
+        for (ProductForSale product : products) {
+            product.showDetails();
+            System.out.println("----------");
+        }
+    }
 
+    public static void main(String[] args) {
+        ProductForSale[] products = new ProductForSale[3];
+        products[0] = new Chocolate("Snack", 5.99, "Milk chocolate", "Milka");
+        products[1] = new Coke("Beverage", 2.99, "Sparkling drink", 330);
+        products[2] = new Bread("Food", 1.99, "Whole wheat bread", 500);
+
+        listProducts(products);
     }
 }
